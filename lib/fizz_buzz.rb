3714f244671e0d1adade9e1bf87175592834c49c
erka <erik.westerberg@hotmail.se>
2019-05-30 19:00:05 +0200

    first commit fizz buzz exercise

---
+++ lib/fizz_buzz.rb
@@ -0,0 +1,17 @@
+def fizz_buzz(number)
+     
+    if has_zero_remainder?(number, 15)
+      'fizz_buzz'
+    elsif has_zero_remainder?(number, 5)
+        'fuzz'
+    elsif has_zero_remainder?(number, 3)
+        'fizz'
+    else
+        number
+    end
+   end
+    
+def has_zero_remainder?(number, divider)
+         number % divider == 0
+    
+      end 
